6578fabea23430de6897af815074b9f27db74380
Yaroslav Donchuk <donchukoff@gmail.com>
2023-08-14 12:45:21 +0300

    Modified .yml file to reun tests earlyer, added blog and resources specs

---
+++ cypress/e2e/blog.spec.cy.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress" />
+
+
+describe('blog page', () => {
+    it('should allow user to visit blog and article', () => {
+      cy.visit('/');
+  
+      
+  
+      cy.contains('.gr-footer__nav-link', 'BLOG')
+        .click();
+  
+      cy.assertPageUrl('/blogs/news');
+  
+      cy.get('h1')
+        .should('contain.text', 'News');
+  
+      cy.get('.gr-blog-search')
+        .should('exist');
+  
+      cy.get('div.gr-main-blog__articles')
+        .find('a')
+        .its('length')
+        .should('be.gt', 1);
+  
+      cy.get('.gr-pagination__list')
+        .should('exist');
+  
+      cy.contains('.gr-article-card__heading', 'Everything You Need to Know to Choose The Right Pallet Truck - 10 Top Tips!')
+        .click();
+  
+      cy.contains('h1', 'Everything You Need to Know to Choose The Right Pallet Truck - 10 Top Tips!')
+        .should('exist');
+  
+      cy.get('.gr-author-info')
+        .should('exist');
+  
+      cy.get('.gr-sharing-wrap')
+        .should('exist');
+  
+      cy.get('div.gr-additional-articles')
+        .find('a')
+        .its('length')
+        .should('be.gt', 1);
+  
+    });
+  });
---
+++ cypress/e2e/resources.spec.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+describe('Resources page', () => {
+    beforeEach(() => {
+      cy.visit('/');
+  
+      
+    });
+    
+    it('should allow user to visit Resources page', () => {
+  
+      cy.contains('.gr-footer__nav-link', 'RESOURCES')
+        .click();
+  
+      cy.assertPageUrl('/pages/resources');
+  
+      cy.get('h1')
+        .should('contain.text', 'Resources');
+  
+      cy.get('div.gr-main-page__content')
+        .find('a')
+        .its('length')
+        .should('be.gt', 1);
+    });
+  
+  });
