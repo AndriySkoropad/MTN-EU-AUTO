7f7d9cf9fc23d262a680e1a3737530e9a4d23268
Yaroslav Donchuk <donchukoff@gmail.com>
2023-08-17 13:39:46 +0300

    Adding tests to the policy link spec

--- cypress/e2e/policyLinks.spec.cy.js
+++ cypress/e2e/policyLinks.spec.cy.js
@@ -22,4 +22,68 @@ describe('should visit all policy links', () => {
       .should('contain.text', 'Terms of Use');
   });
 
+  it('should visit Privacy Policy link', () => {
+
+    cy.contains('.gr-footer__nav-link', 'PRIVACY POLICY')
+      .click();
+
+    cy.assertPageUrl('/pages/privacy-policy');
+
+    cy.get('h1')
+      .should('contain.text', 'Privacy Policy');
+  });
+
+
+  it('should visit Terms of Sale link', () => {
+
+    cy.contains('.gr-footer__nav-link', 'TERMS OF SALE')
+      .click();
+
+    cy.assertPageUrl('/pages/terms-of-sale');
+
+    cy.get('h1')
+      .should('contain.text', 'Terms of Sale');
+  });
+
+  it('should visit Return Policy link', () => {
+
+    cy.contains('.gr-footer__nav-link', 'RETURN POLICY')
+      .click();
+
+    cy.assertPageUrl('/pages/refund-return-policy');
+
+    cy.get('h1')
+      .should('contain.text', 'Refund & Return Policy');
+  });
+
+  it('should visit FAQ link', () => {
+
+    cy.contains('.gr-footer__nav-link', 'FAQS')
+      .click();
+
+    cy.assertPageUrl('/pages/faqs');
+
+    cy.get('h1')
+      .should('contain.text', 'FAQs');
+  });
+
+  it('should visit Our Story link', () => {
+
+    cy.contains('.gr-footer__nav-link', 'OUR STORY')
+      .click();
+
+    cy.assertPageUrl('/pages/about');
+
+    cy.get('h1')
+      .should('contain.text', 'Our Story');
+  });
+
+  it('should visit Home link', () => {
+
+    cy.contains('.gr-footer__nav-link', 'HOME')
+      .click();
+
+    cy.assertPageUrl('/');
+  });
+
 });
